5aa9eb88438fe97a30d4902377869240a3410e26
Hpone Myat Thu <hponemyatthu@192.168.1.11>
2026-02-15 11:42:59 +0630

    clear un-used thing

--- src/layouts/components/topbar/index.tsx
+++ src/layouts/components/topbar/index.tsx
@@ -1,13 +1,9 @@
-
 import { useLayoutContext } from '@/context/useLayoutContext'
-import CustomizerToggler from '@/layouts/components/topbar/components/CustomizerToggler'
 import LanguageDropdown from '@/layouts/components/topbar/components/LanguageDropdown'
-import MegaMenu from '@/layouts/components/topbar/components/MegaMenu'
 import MessageDropdown from '@/layouts/components/topbar/components/MessageDropdown'
-import ThemeToggler from '@/layouts/components/topbar/components/ThemeToggler'
 import UserProfile from '@/layouts/components/topbar/components/UserProfile'
 
-import {Link} from "react-router";
+import { Link } from 'react-router'
 import { Container, FormControl } from 'react-bootstrap'
 import { LuSearch } from 'react-icons/lu'
 import { TbMenu4 } from 'react-icons/tb'
@@ -15,9 +11,6 @@ import { TbMenu4 } from 'react-icons/tb'
 import logoDark from '@/assets/images/logo-black.png'
 import logoSm from '@/assets/images/logo-sm.png'
 import logo from '@/assets/images/logo.png'
-import ApplicationMenu from '@/layouts/components/topbar/components/ApplicationMenu'
-import FullscreenToggle from '@/layouts/components/topbar/components/FullscreenToggle'
-import MonochromeThemeModeToggler from '@/layouts/components/topbar/components/MonochromeThemeModeToggler'
 
 const Topbar = () => {
   const { sidenav, changeSideNavSize, showBackdrop } = useLayoutContext()
@@ -30,7 +23,10 @@ const Topbar = () => {
       html.classList.toggle('sidebar-enable')
       showBackdrop()
     } else if (sidenav.size === 'compact') {
-      changeSideNavSize(currentSize === 'compact' ? 'condensed' : 'compact', false)
+      changeSideNavSize(
+        currentSize === 'compact' ? 'condensed' : 'compact',
+        false,
+      )
     } else {
       changeSideNavSize(currentSize === 'condensed' ? 'default' : 'condensed')
     }
@@ -43,33 +39,39 @@ const Topbar = () => {
           <div className="logo-topbar">
             <Link to="/" className="logo-light">
               <span className="logo-lg">
-                <img src={logo} alt="logo"  />
+                <img src={logo} alt="logo" />
               </span>
               <span className="logo-sm">
-                <img src={logoSm} alt="small logo"  />
+                <img src={logoSm} alt="small logo" />
               </span>
             </Link>
 
             <Link to="/" className="logo-dark">
               <span className="logo-lg">
-                <img src={logoDark} alt="dark logo"  />
+                <img src={logoDark} alt="dark logo" />
               </span>
               <span className="logo-sm">
-                <img src={logoSm} alt="small logo"  />
+                <img src={logoSm} alt="small logo" />
               </span>
             </Link>
           </div>
 
-          <button onClick={toggleSideNav} className="sidenav-toggle-button btn btn-default btn-icon">
+          <button
+            onClick={toggleSideNav}
+            className="sidenav-toggle-button btn btn-default btn-icon"
+          >
             <TbMenu4 className="fs-22" />
           </button>
-
-          <MegaMenu />
         </div>
 
         <div className="d-flex align-items-center gap-2">
           <div className="app-search d-none d-xl-flex me-2">
-            <FormControl type="search" className="topbar-search rounded-pill" name="search" placeholder="Quick Search..." />
+            <FormControl
+              type="search"
+              className="topbar-search rounded-pill"
+              name="search"
+              placeholder="Quick Search..."
+            />
             <LuSearch className="app-search-icon text-muted" />
           </div>
 
@@ -77,17 +79,7 @@ const Topbar = () => {
 
           <MessageDropdown />
 
-          <ApplicationMenu />
-
-          <ThemeToggler />
-
-          <FullscreenToggle />
-
-          <MonochromeThemeModeToggler />
-
           <UserProfile />
-
-          <CustomizerToggler />
         </div>
       </Container>
     </header>
